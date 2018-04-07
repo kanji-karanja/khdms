84342afebe5a69163e2e4a29c58bf67d822aeb3b
kanji-karanja <karimkanji101@gmail.com>
2018-04-07 08:00:05 +0300

    Gear host credentials added

--- home/assets/serve/credentials.php
+++ home/assets/serve/credentials.php
@@ -4,7 +4,10 @@ function errorHandler($errno,$errstr){
 $error_message = "The error experienced is: ".$errstr." and error number is: ".$errstr; 
 }
 set_error_handler("errorHandler");
-$conn=new mysqli("localhost","root","","khdms");
+//Gear host credentials for the gearhost sql server
+$conn=new mysqli("den1.mysql1.gear.host","khdms","Hw5Gp!!a0KVQ","khdms");
+//Local host credentials for localhost sql server
+//$conn=new mysqli("localhost","root","","khdms");
 if($conn->connect_error){
 	die("<div class='alert alert-danger'><strong>Error!</strong> Unable to connect to the Database!".$error_message."</div>");
 }
